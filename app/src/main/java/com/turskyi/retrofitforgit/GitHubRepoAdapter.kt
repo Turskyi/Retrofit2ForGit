85e575e83bfe38cd865e3301884dd1f17da304df
Dmytro Turskyi <dmitriy.turskiy@gmail.com>
2019-11-17 12:52:13 +0200

    update

--- app/src/main/java/com/turskyi/retrofitforgit/GitHubRepoAdapter.kt
+++ app/src/main/java/com/turskyi/retrofitforgit/GitHubRepoAdapter.kt
@@ -6,7 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class GitHubRepoAdapter internal constructor(
+class GitHubRepoAdapter constructor(
     private val repos: List<GitHubRepo>?
 ) :
     RecyclerView.Adapter<GitHubRepoAdapter.ViewHolder>() {
